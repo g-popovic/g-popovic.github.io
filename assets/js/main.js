beb39c39d3d91a8a26b002979a85e506369c1b2d
g-popovic <georgepopovich14@gmail.com>
2023-04-22 10:46:50 +0200

    Misc

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,54 +4,54 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-setTimeout(() => document.querySelector('body').classList.remove('is-preload'), 100);
+setTimeout(() => document.querySelector("body").classList.remove("is-preload"), 100);
 
 function copyEmail(text, event) {
-	var dummy = document.createElement('input');
+	var dummy = document.createElement("input");
 	document.body.appendChild(dummy);
 
-	dummy.setAttribute('id', 'dummy_id');
-	document.getElementById('dummy_id').value = text;
+	dummy.setAttribute("id", "dummy_id");
+	document.getElementById("dummy_id").value = text;
 	dummy.select();
-	document.execCommand('copy');
+	document.execCommand("copy");
 	document.body.removeChild(dummy);
-	event.getElementsByClassName('tooltiptext')[0].innerHTML = 'Copied!';
+	event.getElementsByClassName("tooltiptext")[0].innerHTML = "Copied!";
 }
 
 function resetTooltip(event) {
-	event.getElementsByClassName('tooltiptext')[0].innerHTML = 'Copy';
+	event.getElementsByClassName("tooltiptext")[0].innerHTML = "Copy";
 }
 
-const emailForm = document.getElementById('emailForm');
-emailForm.addEventListener('submit', e => {
+const emailForm = document.getElementById("emailForm");
+emailForm.addEventListener("submit", e => {
 	e.preventDefault();
 
-	const button = document.getElementById('sendButton');
-	const successMessage = document.getElementById('successMessage');
+	const button = document.getElementById("sendButton");
+	const successMessage = document.getElementById("successMessage");
 
-	button.className += ' button-loading';
+	button.className += " button-loading";
 
 	const request = new XMLHttpRequest();
-	request.open('post', 'https://formspree.io/xeqpoowe');
-	request.setRequestHeader('Accept', 'application/json');
+	request.open("post", "https://formspree.io/xeqpoowe");
+	request.setRequestHeader("Accept", "application/json");
 	request.onreadystatechange = () => {
 		if (request.readyState === 4) {
 			if (request.status === 200) {
-				successMessage.classList.remove('hide-message');
-				document.getElementById('message').value = '';
-				document.getElementById('name').value = '';
-				document.getElementById('email').value = '';
+				successMessage.classList.remove("hide-message");
+				document.getElementById("message").value = "";
+				document.getElementById("name").value = "";
+				document.getElementById("email").value = "";
 			} else {
-				alert('Message not sent. There was an unexpected error.');
+				alert("Message not sent. There was an unexpected error.");
 			}
-			button.classList.remove('button-loading');
+			button.classList.remove("button-loading");
 		}
 	};
 	request.send(new FormData(emailForm));
 });
 
 function closeSuccessMessage() {
-	document.getElementById('successMessage').classList += ' hide-message';
+	document.getElementById("successMessage").classList += " hide-message";
 }
 
 // emailForm.addEventListener('submit', e => {
